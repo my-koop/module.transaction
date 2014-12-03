8b5c92e534a6065395810a3f2ae342227558c7fe
Cellule <mike.ferris@hotmail.com>
2014-12-03 12:44:53 -0500

    Made idEvent optional in the typings of NewBill

--- typings/mykoop-transaction/interfaces.d.ts
+++ typings/mykoop-transaction/interfaces.d.ts
@@ -14,7 +14,7 @@ declare module Transaction {
     archiveBill: boolean;
     customerEmail?: string;
     discounts?: Discount[];
-    idEvent: number;
+    idEvent?: number;
     items: {
       id: number;
       price: number;
