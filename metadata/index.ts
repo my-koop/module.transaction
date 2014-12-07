63901d815d4118b52ad790d96dc8b0a6810193be
Mathieu M-Gosselin <mathieumg@gmail.com>
2014-12-07 09:52:18 -0500

    Further work on implementing permissions.

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,37 +1,17 @@
 import utils = require("mykoop-utils");
 import routes = require("./routes");
-var translations = require("../locales/index");
+var translations = require("../locales");
 import endpoints = require("./endpoints");
+import permissions = require("./permissions");
+import contributions = require("./contributions");
 
 var metaDataBuilder = new utils.MetaDataBuilder();
 routes.addRoutes(metaDataBuilder);
 
 metaDataBuilder.addData("translations", translations);
 metaDataBuilder.addData("endpoints", endpoints);
+metaDataBuilder.addData("permissions", permissions);
+metaDataBuilder.addData("contributions", contributions);
 
-metaDataBuilder.addData("adminEditPlugins", {
-  billhistory: {
-    titleKey: "transaction::billHistoryTab",
-    hash: "billhistory",
-    component: {
-      resolve: "component",
-      value: "BillHistoryPage"
-    }
-  }
-});
-
-metaDataBuilder.addData("core", {
-  contributions: {
-    settings: {
-      taxes: {
-        titleKey: "taxes::settingsTitle",
-        component: {
-          resolve: "component",
-          value: "TaxesSettings"
-        }
-      }
-    }
-  }
-});
 var metaData = metaDataBuilder.get();
 export = metaData;
---
+++ metadata/permissions.js
@@ -0,0 +1,12 @@
+var permissions = {
+    invoices: {
+        create: true,
+        read: true,
+        update: true,
+        delete: true,
+        close: true,
+        reopen: true,
+        reports: true
+    }
+};
+module.exports = permissions;
