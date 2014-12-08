5196c6296778c85f45f6de2bec8f2cd8080b31e1
Cellule <mike.ferris@hotmail.com>
2014-12-07 21:17:47 -0500

    can set customer email as a query

--- components/NewBillPage.jsx
+++ components/NewBillPage.jsx
@@ -6,12 +6,15 @@ var __ = require("language").__;
 
 var NewBillPage = React.createClass({
   render: function() {
+    var customerEmail = this.props.query && this.props.query.email;
     return (
       <div>
         <h1>
           {__("transaction::newBillWelcome")}
         </h1>
-        <MKBillDetail />
+        <MKBillDetail
+          billDetails={{customerEmail: customerEmail}}
+        />
       </div>
     );
   }
